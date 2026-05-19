e4b93c330f056f2cd87ec3bf361c4a6ab5e08cbc
Maksym Dudarchuk <129439508+Maksym-Dudar@users.noreply.github.com>
2026-05-19 16:01:08 +0300

    Update payment.service.ts

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -25,7 +25,7 @@ return this.stripeService.createPaymentIntent({
 			amount: Number(order.total) * 100,
 			currency: "usd",
 			metadata: {
-				orderId: order.id,
+  orderId: order.id.toString(),
 			},
 		});
   }
